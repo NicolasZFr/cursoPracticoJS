958f680116f47839264367772b2ac3f1dc377fe9
nicolaszf <nicolaszamf1999@gmail.com>
2022-12-01 14:48:30 -0500

    Respuestas test inicial y archivos de ejemploz

---
+++ script.js
@@ -0,0 +1,17 @@
+// Variables diseñadas para seleccionar las etiquetas, id o clases
+
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const pClass = document.querySelector('.pClass');
+const pId = document.querySelector('#pId');
+const input = document.querySelector('input');
+
+console.log(input.value);
+
+console.log({
+    h1, 
+    p, 
+    pClass, 
+    pId, 
+    input,
+});
