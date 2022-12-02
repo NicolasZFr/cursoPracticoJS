e485c3923c18c74135d9583bfe68d8fc0ae377f5
nicolaszf <nicolaszamf1999@gmail.com>
2022-12-02 17:07:25 -0500

    Se añade elementos del capítulo Escribiendo HTML desde JavaScript - Platzi

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 const pClass = document.querySelector('.pClass');
-const pId = document.querySelector('#pId');
+const pId = document.getElementById('pId');
 const input = document.querySelector('input');
 
 console.log(input.value);
@@ -15,3 +15,23 @@ console.log({
     pId, 
     input,
 });
+
+// Modificar el texto de una etiqueta solo como texto
+
+h1.innerText = 'Manipulación básica del DOM';
+
+//Para obtener el valor de un atributo
+
+console.log(h1.getAttribute('class'));
+
+h1.setAttribute('class', 'rojo');
+
+h1.classList.add('titulo');
+h1.classList.remove('rojo');
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://akamai-webcdn.kgstatic.net/gt/images/open_graph_l.jpg');
+
+pId.replaceWith(img);
+
+img.setAttribute('style', 'height: 30vh');
