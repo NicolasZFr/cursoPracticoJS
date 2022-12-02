02df24214aca58653fa982c138e6db9dba2ae8aa
nicolaszf <nicolaszamf1999@gmail.com>
2022-12-02 18:59:13 -0500

    Se aplican enseñanzas de la clase de Eventos en JavaScript

--- script.js
+++ script.js
@@ -1,37 +1,21 @@
 // Variables diseñadas para seleccionar las etiquetas, id o clases
 
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const pClass = document.querySelector('.pClass');
 const pId = document.getElementById('pId');
-const input = document.querySelector('input');
-
-console.log(input.value);
-
-console.log({
-    h1, 
-    p, 
-    pClass, 
-    pId, 
-    input,
-});
-
-// Modificar el texto de una etiqueta solo como texto
-
-h1.innerText = 'Manipulación básica del DOM';
-
-//Para obtener el valor de un atributo
-
-console.log(h1.getAttribute('class'));
-
-h1.setAttribute('class', 'rojo');
-
-h1.classList.add('titulo');
-h1.classList.remove('rojo');
+const pResultado = document.getElementById('resultado');
+const input1 = document.querySelector('#input-ejemplo1');
+const input2 = document.querySelector('#input-ejemplo2');
+const button = document.querySelector('#btn-calcular');
 
 const img = document.createElement('img');
 img.setAttribute('src', 'https://akamai-webcdn.kgstatic.net/gt/images/open_graph_l.jpg');
 
-pId.replaceWith(img);
+pId.innerHTML= "";
+pId.append(img);
 
 img.setAttribute('style', 'height: 30vh');
+
+function btnCalcular() {
+    let valorTotal = Number(input1.value) + Number(input2.value);
+    pResultado.innerText = `La suma total de ${input1.value} y ${input2.value} da como resultado ${valorTotal}`;
+}
